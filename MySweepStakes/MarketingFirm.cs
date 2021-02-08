6dc04850d2dbc72f061e62aa8bea3129e92d0000
Dustin <adustin70@gmail.com>
2021-02-08 16:33:23 -0500

    Added CreateSweepstakes method to MarketingFirm class

--- MySweepStakes/MarketingFirm.cs
+++ MySweepStakes/MarketingFirm.cs
@@ -8,5 +8,22 @@ namespace MySweepStakes
 {
     class MarketingFirm
     {
+        ISweepstakesManager _manager;
+
+        public MarketingFirm(ISweepstakesManager manager)
+        {
+            this._manager = manager;
+        }
+
+        public void CreateSweepstakes()
+        {
+            Contestant contestant = new Contestant();
+            contestant.GetContestantInfo();
+
+            Sweepstakes sweepstakes = new Sweepstakes(contestant.ContestantFirstName);
+            sweepstakes.RegisterContestant(contestant);
+            sweepstakes.PickWinner();
+            sweepstakes.PrintContestantInfo(contestant);
+        }
     }
 }
