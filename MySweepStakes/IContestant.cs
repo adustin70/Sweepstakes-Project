6d297592a27a4a717a434c8a4d4ea4866b9b27d4
Dustin <adustin70@gmail.com>
2021-02-08 11:03:25 -0500

    Added variables and constructor to IContestant class

--- MySweepStakes/IContestant.cs
+++ MySweepStakes/IContestant.cs
@@ -8,6 +8,19 @@ namespace MySweepStakes
 {
     class IContestant
     {
+        public string ContestantFirstName { get; set; }
+        public string ContestantLastName { get; set; }
+        public string ContestantEmail { get; set; }
+        public int ContestantRegistation { get; set; }
+
+        public IContestant(string firstName, string lastName, string eMail, int registrationNumber)
+        {
+            this.ContestantFirstName = firstName;
+            this.ContestantLastName = lastName;
+            this.ContestantEmail = eMail;
+            this.ContestantRegistation = registrationNumber;
+        }
+
 
     }
 }
