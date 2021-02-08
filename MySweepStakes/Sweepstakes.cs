a4ac8af18c1d49ab55d6e9cef2efe675df7df168
Dustin <adustin70@gmail.com>
2021-02-08 14:13:34 -0500

    Added variables, constructor and methods to Sweepstakes class

--- MySweepStakes/Sweepstakes.cs
+++ MySweepStakes/Sweepstakes.cs
@@ -8,5 +8,43 @@ namespace MySweepStakes
 {
     class Sweepstakes
     {
+        Dictionary<int, Contestant> contestants;
+        public string Name { get; set; }
+
+        public Sweepstakes(string name)
+        {
+            name = Name;
+            contestants = new Dictionary<int, Contestant>();
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+            for (int key = 0; key < contestants.Count; key++)
+            {
+                contestants.Add(key, contestant);
+            }
+        }
+
+        public Contestant PickWinner()
+        {
+            Random randomKey = new Random();
+            int key = randomKey.Next(contestants.Count);
+
+            foreach (KeyValuePair<int, Contestant> contestant in contestants)
+            {
+                if (key == contestant.Key)
+                    return contestant.Value;
+            }
+            return null;
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            foreach (KeyValuePair<int, Contestant> theContestant in contestants)
+            {
+                if (contestant == theContestant.Value)
+                Console.WriteLine(theContestant.Key + "-" + theContestant.Value);
+            }
+        }
     }
 }
