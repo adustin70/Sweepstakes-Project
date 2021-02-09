a1ae41d3af2e37be1380e447a601b61a613e86d8
Dustin <adustin70@gmail.com>
2021-02-09 14:55:33 -0500

    Added comments to Simulation class about where and why I used dependency injection

--- MySweepStakes/Simulation.cs
+++ MySweepStakes/Simulation.cs
@@ -9,7 +9,8 @@ namespace MySweepStakes
     class Simulation
     {
         public void CreateMarketingFirmWithManager()
-        {
+        {// I used method dependency injection here so that based off of user input it will set the manager to be used and pass it into MarketingFirm
+        //  Which will create a new instance of MarketingFirm with eiter a Stack or Queue manager
             if (UserInterface.AskForStackOrQueue() == true)
             {
                 ISweepstakesManager manager = new SweepstakeStackManager();
